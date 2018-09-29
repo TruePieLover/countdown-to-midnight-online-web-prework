a213d884d2da3f35fc4bb06ae02f988af595c3d7
Brandon Teich <brandonteich123@gmail.com>
2018-09-29 03:03:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number < 0
+ 
+    puts "#{number} SECOND(S)!"
+     number - 1
+   end
+end
+
+x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
 end
